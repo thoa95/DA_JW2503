bf589ff96bee60250739cd040e6f769015b4b949
thoant <thoa@gmail.com>
2025-10-14 08:09:02 +0700

    change DDL sql

--- src/main/java/com/bkap/qlks/entity/Account.java
+++ src/main/java/com/bkap/qlks/entity/Account.java
@@ -12,48 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Account {
 	@Id
-	private String name;
+	private String accountId;
 	private String password;
 	private Integer gender;
 	private String phone;
 	private String email;
-	private Integer isAdmin;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Integer getGender() {
-		return gender;
-	}
-	public void setGender(Integer gender) {
-		this.gender = gender;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Integer getIsAdmin() {
-		return isAdmin;
-	}
-	public void setIsAdmin(Integer isAdmin) {
-		this.isAdmin = isAdmin;
-	}
+	private String role;
+	
 	
 	
 }
