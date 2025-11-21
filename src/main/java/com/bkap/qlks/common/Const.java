ad70ecc1078e11585c75d697eba7d28bcfb30631
thoant <thoa@gmail.com>
2025-11-21 18:18:52 +0700

    fix config port

--- src/main/java/com/bkap/qlks/common/Const.java
+++ src/main/java/com/bkap/qlks/common/Const.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Const {
-	public final static Integer size = 15;
+	public final static Integer size = 9;
 	public final static String VNPAY_METHOD = "VNPAY";
 	public final static String CASH_METHOD = "CASH";
 }
